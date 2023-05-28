71d961222495420d40592839bd17760708105b33
EG Adams <eg1972@gmail.com>
2023-05-28 19:44:51 -0400

    added run, arun and Field

--- gpt_server/code_gen/GenerateDirectories.py
+++ gpt_server/code_gen/GenerateDirectories.py
@@ -1,8 +1,10 @@
 import os
 import logging
 from langchain.tools.base import BaseTool
+from pydantic import BaseModel, Field
 
 class GenerateDirectories(BaseTool):
+    base_directory: str = Field(...)
     def __init__(self, base_directory):
         self.base_directory = base_directory
         logging.basicConfig(level=logging.INFO)
@@ -36,7 +38,14 @@ class GenerateDirectories(BaseTool):
         else:
             logging.error(f"Failed to create directory '{directory_name}'. Invalid directory name or directory already exists.")
             return False
+    def _run(self, *args, **kwargs):
+        # Implement the synchronous execution logic here
+        print( "*** WARNING: GenerateDirectories._run() called, but not implemented. ***" )
 
+    async def _arun(self, *args, **kwargs):
+        # Implement the asynchronous execution logic here
+        print( "*** WARNING: GenerateDirectories._arun() called, but not implemented. ***" )
+    
 # if main
 if __name__ == "__main__":
     base_dir = 'code_gen'
